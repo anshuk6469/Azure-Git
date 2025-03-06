878dd87923d07510f739ab4d80468ee4e173451d
Deepanshu Kumar <anshuk6469@gmail.com>
2025-03-06 15:57:58 +0530

    rg.tf updated

--- rg.tf
+++ rg.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "example1" {
   name = "deep-git2"
   location = "eastus"
 }
+resource "azurerm_resource_group" "example2" {
+  name = "deep-git3"
+  location = "eastus"
+}
+
