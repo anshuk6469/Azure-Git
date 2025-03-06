f8977cc7b382fca48eb80af05688af4a819ce3fb
Deepanshu Kumar <anshuk6469@gmail.com>
2025-03-06 16:55:22 +0530

    Removed all

--- rg.tf
+++ rg.tf
@@ -1,17 +1,17 @@
-resource "azurerm_resource_group" "example" {
-  name = "deep-git1"
-  location = "eastus"
-}
+# resource "azurerm_resource_group" "example" {
+#   name = "deep-git1"
+#   location = "eastus"
+# }
 
-resource "azurerm_resource_group" "example1" {
-  name = "deep-git2"
-  location = "eastus"
-}
-resource "azurerm_resource_group" "example2" {
-  name = "deep-git3"
-  location = "eastus"
-}
-resource "azurerm_resource_group" "example3" {
-  name = "deep-git4"
-  location = "eastus"
-}
+# resource "azurerm_resource_group" "example1" {
+#   name = "deep-git2"
+#   location = "eastus"
+# }
+# resource "azurerm_resource_group" "example2" {
+#   name = "deep-git3"
+#   location = "eastus"
+# }
+# resource "azurerm_resource_group" "example3" {
+#   name = "deep-git4"
+#   location = "eastus"
+# }
