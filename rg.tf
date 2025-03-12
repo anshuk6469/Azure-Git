3d3d91684fc4aad36756cf094a74b6370b6fd4d1
Deepanshu Kumar <86040514+anshuk6469@users.noreply.github.com>
2025-03-12 15:41:29 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,7 +1,7 @@
-# resource "azurerm_resource_group" "example" {
-#   name = "deep-git1"
-#   location = "eastus"
-# }
+resource "azurerm_resource_group" "example" {
+   name = "deep-git1"
+   location = "eastus"
+ }
 
 # resource "azurerm_resource_group" "example1" {
 #   name = "deep-git2"
